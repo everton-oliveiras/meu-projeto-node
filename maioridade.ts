f399965d57b6933e40eb0aa5874703967cee84f8
Everton Oliveira <everton.oliveira@webropay.com.br>
2026-01-19 10:04:19 -0300

    Criado arquivo de exercício de Maioridade

---
+++ maioridade.ts
@@ -0,0 +1,18 @@
+/*
+1) Maioridade
+Enunciado: Dada uma idade, retorne "Maior de idade" se idade >= 18, senão "Menor de idade".
+
+Exemplo:
+Entrada: idade = 16 → Saída: "Menor de idade"
+Entrada: idade = 20 → Saída: "Maior de idade"
+
+*/
+
+const idade: number = 20;
+
+if (idade >= 18) {
+    console.log("Maior de idade");
+}
+else {
+    console.log("Menor de idade");
+}
