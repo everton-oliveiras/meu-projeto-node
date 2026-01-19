620f8ec35e2364c7d720dfcd0ca0cc1d652f0b74
Everton Oliveira <everton.oliveira@webropay.com.br>
2026-01-19 18:56:11 -0300

    Exercício 10 - contagem de ocorrencias

---
+++ contar-ocorrencias.ts
@@ -0,0 +1,34 @@
+/*
+    10) Contar ocorrências
+    Enunciado: Dado um array e um valor x, retorne quantas vezes x aparece.
+
+    Exemplo:
+    Entrada: arr=[1,2,2,2,3], x=2 → Saída: 3
+
+*/
+
+const listaNumeros: number[] = [1, 2, 2, 2, 2, 2, 2, 3];
+const x: number = 2;
+let contagem: number = 0;
+
+// for (const num of listaNumeros) {
+//     if (num === x) {
+//         contagem = contagem + 1;
+//     }
+// }
+
+for (let i: number = 1; i <= listaNumeros.length; i++) {
+
+    console.log(`Valor de I: ${i}`);
+
+    console.log(`Valor do elemento ${i} no array listaNumeros ${listaNumeros[i - 1]}`);
+
+    // if (listaNumeros[i] === x) {
+    //     contagem = contagem + 1;
+    // }
+}
+
+// console.log(listaNumeros[-1]);
+
+
+console.log(`O número ${x} apareceu ${contagem} vezes na lista.`);
