734ef0e9defb066759604590081c1528a8b7e8d4
Everton Oliveira <everton.oliveira@webropay.com.br>
2026-01-19 16:19:38 -0300

    Resolução do exercício 07 - Feito e explicado pelo Moisés.

--- retornar-pares.ts
+++ retornar-pares.ts
@@ -7,4 +7,27 @@
 
 */
 
-const numeros: number[] = [1, 2, 3, 4, 5, 6];
+//Solução 1
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numeroPares: number[] = [];
+
+for (const banana of numeros) {
+    console.log(`Lendo número ${banana}`);
+
+    if (banana % 2 === 0) {
+        numeroPares.push(banana);
+        console.log(`Número ${banana} é par e foi adicionado na lista de número pares`)
+    }
+    else
+    {
+        console.log(`O número ${banana} não é par`);
+    }
+}
+
+//Solução 2
+
+const pares = numeros.filter((numero) => {
+    return numero % 2 === 0;
+});
+
+console.log(pares);
