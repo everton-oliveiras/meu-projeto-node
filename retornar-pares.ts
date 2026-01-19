91bc143898908ee57af5c7dd8a51acde0c1ef870
Everton Oliveira <everton.oliveira@webropay.com.br>
2026-01-19 14:58:05 -0300

    feat/Inicio do exercicio de Array.

---
+++ retornar-pares.ts
@@ -0,0 +1,10 @@
+/*
+    7) Retornar pares
+    Enunciado: Dado um array numérico, retorne um novo array só com os números pares.
+
+    Exemplo:    
+    Entrada: [1,2,3,4,5,6] → Saída: [2,4,6]
+
+*/
+
+const numeros: number[] = [1, 2, 3, 4, 5, 6];
