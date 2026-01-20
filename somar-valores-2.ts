87ebcf74c6ae7eef0f5a32d777650e991599cf46
Everton Oliveira <everton.bps@gmail.com>
2026-01-20 17:33:53 -0300

    Exercício 08 refeito para fixação e entendimento

---
+++ somar-valores-2.ts
@@ -0,0 +1,7 @@
+const numerosLista: number[] = [15, 15, 30, 10, 5, 15, 15, 5];
+let resultadoSoma: number = 0;
+
+for (let num of numerosLista) {
+    resultadoSoma = resultadoSoma + num;
+}
+console.log(resultadoSoma);
