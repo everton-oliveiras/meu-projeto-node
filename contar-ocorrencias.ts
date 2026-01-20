76bb6630f716f97a7026076cf6d4b8b4f1a6644a
Everton Oliveira <everton.oliveira@webropay.com.br>
2026-01-20 15:22:50 -0300

    Criação de arquivos para aprender objetos

--- contar-ocorrencias.ts
+++ contar-ocorrencias.ts
@@ -11,22 +11,22 @@ const listaNumeros: number[] = [1, 2, 2, 2, 2, 2, 2, 3];
 const x: number = 2;
 let contagem: number = 0;
 
-// for (const num of listaNumeros) {
-//     if (num === x) {
-//         contagem = contagem + 1;
-//     }
-// }
-
-for (let i: number = 1; i <= listaNumeros.length; i++) {
-
-    console.log(`Valor de I: ${i}`);
-
-    console.log(`Valor do elemento ${i} no array listaNumeros ${listaNumeros[i - 1]}`);
-
-    // if (listaNumeros[i] === x) {
-    //     contagem = contagem + 1;
-    // }
+for (const num of listaNumeros) {
+    if (num === x) {
+        contagem = contagem + 1;
+    }
 }
+
+// for (let i: number = 1; i <= listaNumeros.length; i++) {
+
+//     console.log(`Valor de I: ${i}`);
+
+//     console.log(`Valor do elemento ${i} no array listaNumeros ${listaNumeros[i - 1]}`);
+
+//     // if (listaNumeros[i] === x) {
+//     //     contagem = contagem + 1;
+//     // }
+// }
 
 // console.log(listaNumeros[-1]);
 
